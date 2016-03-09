38a3f149fa993626a1c904a32fd1e00be6e90cac
gkf6493 <gyw_kdn@163.com>
2016-03-09 15:34:57 +0800

    Initial commit

---
+++ src/main/java/com/gyw/web/MyController.java
@@ -0,0 +1,23 @@
+package com.gyw.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * TODO 控制层代码
+ */
+@Controller
+public class MyController {
+
+    @RequestMapping("login") //用来处理前台的login请求
+    private @ResponseBody String hello(
+            @RequestParam(value = "username", required = false)String username,
+            @RequestParam(value = "password", required = false)String password
+    ){
+        return "Hello "+username+",Your password is: "+password;
+
+    }
+
+}
